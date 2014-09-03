9854d104e3273079f7bf729e037afbc90f37e92f
jpablosd <juanpablo@MacBook-Air-de-Juan.local>
2014-09-03 15:13:30 -0400

    versión 0.1.9
    Se suman los archivos ReleaseNotes.txt y UserGuide.txt, los cuales van
    incluidos en la misma versión 0.1.9

--- Arduino/awarehome_0_1/awarehome_0_1.ino
+++ Arduino/awarehome_0_1/awarehome_0_1.ino
@@ -7,7 +7,9 @@
 dht DHT;
 
 byte mac[] = {  0x90, 0xA2, 0xDA, 0x0D, 0x4E, 0xD7 }; // MAC de la tarjeta ethernet shield
-byte server[] = { 192,168,1,158 }; // Direccion ip del servidor
+byte server[] = { 190,153,212,77 }; // Direccion ip del servidor
+
+
 
 EthernetClient client;
 float temperatura;
@@ -28,7 +30,7 @@ void setup()
     Serial.print("."); 
 }
   Serial.println();
-  delay(500); // espera 1 segundo despues de inicializar
+  delay(1000); // espera 1 segundo despues de inicializar
 }
 
 void loop()
@@ -47,13 +49,15 @@ void loop()
   if (client.connected()) {
     Serial.println("Enviando Datos al Servidor");
     
-    
-    client.print("GET /servidor/arduino/arduino.php?id_usuario=1");
+    client.print("GET /awarehome/arduino/Arduino.php?id_usuario=1");
     client.print("&nombre_sensor=TyH");
     client.print("&temperatura=");
     client.print(temperatura);
     client.print("&humedad=");
     client.print(humedad);
+  
+    //Serial.print(temperatura);
+    //Serial.print(humedad);
   
     client.println(" HTTP/1.0");
     client.println("User-Agent: Arduino 1.0");
@@ -64,5 +68,5 @@ void loop()
   }
   client.stop();
   client.flush();
-  delay(5000); // espera 5 segundos antes de volver a sensar la temperatura
+  delay(5000); // espera 10 segundos antes de volver a sensar la temperatura
 }
